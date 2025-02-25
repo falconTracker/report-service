1689e33b2e3efc54a7e8fa7702f772a3acca02f3
leopordlau <2279846752@qq.com>
2025-02-25 23:55:41 +0800

    chore: 修改脚本

--- script/mongodb-restart.sh
+++ script/mongodb-restart.sh
@@ -3,6 +3,5 @@
 
 #本地
 /usr/local/mongodb/bin/mongod -f /usr/local/etc/mongod27017.conf
-/usr/local/mongodb/bin/mongod -f /usr/local/etc/mongod27018.conf
 
 /usr/local/redis/src/redis-server /usr/local/redis/redis.conf
